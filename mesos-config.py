91b0729ed27eea169a9bb14baa27b8e1ecf1d800
nandagirin <nandagirin17@gmail.com>
2018-08-03 10:28:09 +0700

    initial test

--- mesos-config.py
+++ mesos-config.py
@@ -1,2 +1,8 @@
-import json
+from subprocess import call
+import math, json, sys, os, argparse, subprocess, apt
+
+print ("**************************************************")
+print ("**          Tested on Ubuntu 16.04 LTS          **")
+print ("**             Mesos version 1.6.0              **")
+print ("**************************************************")
 
