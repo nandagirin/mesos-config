ef5e7b8ce41fa675f10c90a759bbfbad9405ef94
nandagirin <nandagirin17@gmail.com>
2018-08-03 14:00:10 +0700

    1

--- mesos-config.py
+++ mesos-config.py
@@ -1,8 +1,21 @@
-from subprocess import call
-import math, json, sys, os, argparse, subprocess, apt
+from subprocess import *
+import json, sys, os, argparse, subprocess
 
 print ("**************************************************")
 print ("**          Tested on Ubuntu 16.04 LTS          **")
 print ("**             Mesos version 1.6.0              **")
 print ("**************************************************")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', '-c',
+    dest = 'config', help = 'Config written in JSON', required = True)
+args = parser.parse_args()
+
+cfgfile = open(str(os.getcwd()) + "/" + str(args.config))
+conf = json.load(cfgfile)
+
+print("Config file:\n" + json.dumps(conf, indent=2))
+mastercount = len(conf['MASTERS'])
+slavecount = len(conf['SLAVES'])
+quorum = (2 * mastercount) - 1
 
